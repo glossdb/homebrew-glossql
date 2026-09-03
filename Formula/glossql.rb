3b3506cae17138a9309f2ca2db4645e27028ca67
phas02 <suter.philipp@gmail.com>
2026-09-03 10:12:12 +0200

    v0.1.1

--- Formula/glossql.rb
+++ Formula/glossql.rb
@@ -3,19 +3,19 @@
 class Glossql < Formula
   desc "Context language server: SQL-shaped surface over a workspace's data and context"
   homepage "https://github.com/glossdb/glossql"
-  url "https://github.com/glossdb/glossql/releases/download/v0.1.0/glossql-0.1.0-aarch64-apple-darwin.tar.gz"
-  sha256 "3a4b31e79abc42351decf411fc180e69a2a552f6c925dee72735c04a7d3dc59b"
-  version "0.1.0"
+  url "https://github.com/glossdb/glossql/releases/download/v0.1.1/glossql-0.1.1-aarch64-apple-darwin.tar.gz"
+  sha256 "9a4ef83a942967098f2b301ba0257b236037036c3370aff388cfedc100ef4bbc"
+  version "0.1.1"
   license "Apache-2.0"
 
   depends_on :macos
   depends_on arch: :arm64
 
   def install
-    bin.install "serverd"
+    bin.install "glossql"
   end
 
   test do
-    assert_match version.to_s, shell_output("#{bin}/serverd --version")
+    assert_match version.to_s, shell_output("#{bin}/glossql --version")
   end
 end
