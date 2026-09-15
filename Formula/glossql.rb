d9bc6216686586c8d480f3fb7569de6225cd2bf2
phas02 <suter.philipp@gmail.com>
2026-09-15 18:45:48 +0200

    glossql 0.1.5

--- Formula/glossql.rb
+++ Formula/glossql.rb
@@ -3,9 +3,9 @@
 class Glossql < Formula
   desc "Context language server: SQL-shaped surface over a workspace's data and context"
   homepage "https://github.com/glossdb/glossql"
-  url "https://github.com/glossdb/glossql/releases/download/v0.1.4/glossql-0.1.4-aarch64-apple-darwin.tar.gz"
-  sha256 "0f9a1842ca12edeea6d6839119fa7fa428c23adb702b59236a9cee057bc4ae2b"
-  version "0.1.4"
+  url "https://github.com/glossdb/glossql/releases/download/v0.1.5/glossql-0.1.5-aarch64-apple-darwin.tar.gz"
+  sha256 "36966ff2c5685dec6505e2969b0e8f916f86e8bbf3f58f9bfe689fe7ab29fb8e"
+  version "0.1.5"
   license "Apache-2.0"
 
   depends_on :macos
